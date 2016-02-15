e96ce1c228106b98b12fa5c23301de3317d7a9ea
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-02-15 11:53:03 -0800

    Initial Commit

---
+++ Project 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 3
+//
+//  Created by Gopi Suresh on 2/15/16.
+//  Copyright © 2016 Gopi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
